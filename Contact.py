cb9cba890aa7daa7dece2eaed8b25c375dcfb017
Natalia <bnatasha559@gmail.com>
2025-03-21 16:43:41 +0300

    Тест для контактов

---
+++ Contact.py
@@ -0,0 +1,7 @@
+class Contact:
+
+    def __init__(self, firstname, lastname, home_phone, email):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.home_phone = home_phone
+        self.email = email
