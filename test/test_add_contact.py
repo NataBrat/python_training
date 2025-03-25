a15a9a252ced946d365ca6b3e5a610a37eed8cd3
Natalia <bnatasha559@gmail.com>
2025-03-25 22:20:27 +0300

    Добавлено поле адрес для контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,12 +5,12 @@ from model.contact import Contact
 
 def test_add_contact(app):
     app.session.login(username = "admin", password = "secret")
-    app.contact.create(Contact("Ivan", "Ivanov", "223344", "ivan@test.test"))
+    app.contact.create(Contact("Ivan", "Ivanov", "Lenina 5","223344", "ivan@test.test"))
     app.session.logout()
 
 
 def test_add_empty_contact(app):
     app.session.login(username = "admin", password = "secret")
-    app.contact.create(Contact(firstname = "", lastname = "", home_phone = "", email = ""))
+    app.contact.create(Contact(firstname = "", lastname = "", address = "", home_phone = "", email = ""))
     app.session.logout()
 
