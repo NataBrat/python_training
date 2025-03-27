147b785a40e36b9bc1b339766eb81479ef03e89e
Natalia <bnatasha559@gmail.com>
2025-03-27 19:47:14 +0300

    Добавлены предусловия в тестах для удаления и модификации для групп и контактов

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,7 +2,11 @@ from model.contact import Contact
 
 
 def test_modify_contact_name(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(lastname="test"))
     app.contact.modify_first_contact(Contact(firstname="New name"))
 
 def test_modify_contact_email(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.modify_first_contact(Contact(email="New email"))
