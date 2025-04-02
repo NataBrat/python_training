00d1fa59f9e238da01dbd266a033152f40f59c2a
Natalia <bnatasha559@gmail.com>
2025-04-03 00:07:56 +0300

    Параметризация тестов для создания групп и контактов со случайными строками

--- fixture/group.py
+++ fixture/group.py
@@ -46,23 +46,6 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
 
-#    def edit_first_group(self, group):
-#        wd = self.app.wd
-#        self.open_groups_page()
-#        self.select_first_group()
-#        # submit editing
-#        wd.find_element_by_name("edit").click()
-#        # editing group form
-#        wd.find_element_by_name("group_name").click()
-#        wd.find_element_by_name("group_name").send_keys(group.name)
-#        wd.find_element_by_name("group_header").click()
-#        wd.find_element_by_name("group_header").send_keys(group.header)
-#        wd.find_element_by_name("group_footer").click()
-#        wd.find_element_by_name("group_footer").send_keys(group.footer)
-#        # submit group editing
-#        wd.find_element_by_name("update").click()
-#        self.return_to_groups_page()
-
     def create(self, group):
         wd = self.app.wd
         self.open_groups_page()
@@ -102,12 +85,12 @@ class GroupHelper:
     group_cache = None
 
     def get_group_list(self):
+        wd = self.app.wd
         if self.group_cache is None:
-            wd = self.app.wd
             self.open_groups_page()
             self.group_cache = []
-        for element in wd.find_elements_by_css_selector("span.group"):
-            text = element.text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            self.group_cache.append(Group(name=text,id=id))
+            for element in wd.find_elements_by_css_selector("span.group"):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.group_cache.append(Group(name=text,id=id))
         return list(self.group_cache)
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,24 +1,36 @@
 # -*- coding: utf-8 -*-
+import pytest
 
 from model.contact import Contact
 
+import random
+import string
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    result = prefix + "".join([random.choice(symbols) for _ in range(random.randrange(maxlen))])
+    # Заменяем двойные пробелы и удаляем пробелы в конце для уменьшения падений теста
+    return result.replace("  ", " ").rstrip()
+
+testdata = [Contact(firstname="", lastname="", address="", home="", mobile="",
+                    work="", email1="", email2="", email3="")] + [
+    Contact(firstname=random_string("firstname", 10),
+            lastname=random_string("lastname", 10),
+            address=random_string("address", 10),
+            home=random_string("home", 10),
+            mobile=random_string("mobile", 10),
+            work=random_string("work", 10),
+            email1=random_string("email_1", 10),
+            email2=random_string("email_2", 10),
+            email3=random_string("email_3", 10))
+    for i in range(3)
+]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Ivan", lastname= "Ivanov", address="Lenina 5", home="22 33 44", mobile="55 55 55",
-                      work="99 99 99", email1="ivan1@test.test", email2= "ivan2@test.test", email3="ivan3@test.test")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-
-#def test_add_empty_contact(app):
-#    old_contacts = app.contact.get_contact_list()
-#    contact = Contact(firstname = "", lastname = "", address = "", home = "", mobile = "", work = "", email1 = "", email2="", email3="")
-#    app.contact.create(contact)
-#    new_contacts = app.contact.get_contact_list()
-#    assert len(old_contacts) + 1 == len(new_contacts)
-#    old_contacts.append(contact)
-#    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
